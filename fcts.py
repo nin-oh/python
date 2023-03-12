d9837b955f86d710681a9bfa628dfc029813258c
othmane <othmane>
2023-03-12 22:59:55 +0100

    ex7

--- fcts.py
+++ fcts.py
@@ -1,15 +1,12 @@
-def srta(mot):
-    t=[]
-    m=""
-    j=0
-    for i in range(len(mot)):
-          
-          if mot[i] =="-" or i==len(mot)-1:
-            m=mot[j:i]
-            t.append(m)
-            j=i+1
-    t=sorted(t)
-    
-    return '-'.join(t);
-
-print(srta("plipu-jfkghi-fgdfg"))
+def dic(dic1,dic2):
+     dic={}
+     for i in dic1:
+          dic[i]=dic1[i]
+     for j in dic2.keys() :
+          if dic.get(j,"non")=="non":
+               dic[j]=dic2[j]
+          else :
+               dic[j]+=dic2[j]
+     return dic           
+     
+print(dic({'a':10,'b':20},{'a':20,'b':30,'d':40}))
