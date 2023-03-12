f3cbc45b329bb85ae82e8b0e7974fd50e00d1c0a
othmane <othmane>
2023-03-12 22:57:02 +0100

    ex3

--- fcts.py
+++ fcts.py
@@ -1,7 +1,14 @@
-def fon(liste):
-    v=[]
-    for value in liste :
-        if value%2==0 :
-            v.append(value);
-    return v;
-print(fon([1,5,6,7]))
+def somli(liste1,liste2):
+    li=[]
+    if   len(liste1) >= len(liste2) :
+        for i in range(len(liste1)):
+            if i<len(liste2):
+               li.append(liste1[i]+liste2[i])
+            else : li.append(liste1[i])
+    else  :
+        for i in range(len(liste2)):
+            if i<len(liste1):
+               li.append(liste1[i]+liste2[i])
+            else : li.append(liste2[i])
+    return li;
+print(somli([1,2,3],[1,2,3]))
