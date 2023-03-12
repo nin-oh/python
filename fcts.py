110a0ab414e589bc58436f42ec6eab4a894abb81
othmane <othmane>
2023-03-12 22:57:40 +0100

    ex4

--- fcts.py
+++ fcts.py
@@ -1,14 +1,6 @@
-def somli(liste1,liste2):
-    li=[]
-    if   len(liste1) >= len(liste2) :
-        for i in range(len(liste1)):
-            if i<len(liste2):
-               li.append(liste1[i]+liste2[i])
-            else : li.append(liste1[i])
-    else  :
-        for i in range(len(liste2)):
-            if i<len(liste1):
-               li.append(liste1[i]+liste2[i])
-            else : li.append(liste2[i])
-    return li;
-print(somli([1,2,3],[1,2,3]))
+def milli(liste,n):
+    for i in range(len(liste)):
+        liste[i]=liste[i]*n;
+li=[1,2,3]
+milli(li,2)
+print(li)
