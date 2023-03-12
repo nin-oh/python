6753d56efbd5ed4c74205c7ae13cfb0f6a46afa5
othmane <othmane>
2023-03-12 22:56:03 +0100

    ex2

--- fcts.py
+++ fcts.py
@@ -1,6 +1,7 @@
-def baex(b,n):
-    s=1
-    for value in range(n):
-         s*=b
-    return s;
-print(baex(2,3))
+def fon(liste):
+    v=[]
+    for value in liste :
+        if value%2==0 :
+            v.append(value);
+    return v;
+print(fon([1,5,6,7]))
